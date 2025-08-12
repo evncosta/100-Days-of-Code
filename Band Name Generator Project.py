e309804e9e1751e051733a0801da9eb169d80ca9
Esther Vila Nova <esthervilanovacosta@gmail.com>
2025-08-12 14:23:09 -0300

    Update and rename Projeto Dia 1.py to Band Name Generator Project.py

---
+++ Band Name Generator Project.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("Suggest a name for a pet!\n")
+
+print("Your band name could be " + city + " " + pet)
