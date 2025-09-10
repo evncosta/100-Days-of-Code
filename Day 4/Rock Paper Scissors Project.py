5647469e0391fe7897bf1e0a4a40b22051e492e8
Esther Vila Nova <esthervilanovacosta@gmail.com>
2025-09-10 12:51:18 -0300

    Update Rock Paper Scissors Project.py

--- Day 4/Rock Paper Scissors Project.py
+++ Day 4/Rock Paper Scissors Project.py
@@ -1,5 +1,7 @@
+# Rock Paper Scissors Game - Player vs Computer
 import random
 
+# ASCII art for rock, paper, scissors
 choices = ['''
     _______
 ---'   ____)
@@ -25,15 +27,18 @@ choices = ['''
 ---.__(___)
 ''']
 
+# Get player choice and generate computer choice
 player_turn = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
 comp_turn = random.randint(0, 2)
 
+# Display choices using ASCII art
 print("You chose:")
 print(choices[player_turn])
 
 print("The computer chose:")
 print(choices[comp_turn])
 
+# Determine winner using modulo arithmetic
 if player_turn == comp_turn:
     print("It's a tie.")
 elif (player_turn - comp_turn) % 3 == 1:
